9dba080ec45f0ac00a1e6a7204ab1ec12c7c34b6
samannetts8 <samannetts8@gmail.com>
2025-03-20 14:02:54 +0000

    Passed 2nd Example Test

--- webpage_scraper.test.js
+++ webpage_scraper.test.js
@@ -39,13 +39,16 @@ describe("Longitude, Latitude Retrieval", () => {
   it("should return the whole Google Maps element", () => {
     //Arrange
     const html_file = fs.readFileSync(
-      new URL("./packwood_house_page_source.html", import.meta.url),
+      new URL(
+        "./middle_littleton_tithe_barn_page_source.html",
+        import.meta.url
+      ),
       "utf8"
     );
     //Act
     const result = webpage_scraper(html_file);
     //Assert
-    const answer = `<a href="https://www.google.com/maps/dir/?api=1&amp;destination=52.3465%2C-1.74551" id="propertyViewOnGoogleMaps_image"></a>`;
+    const answer = `<a href="https://www.google.com/maps/dir/?api=1&amp;destination=52.1213%2C-1.88429" id="propertyViewOnGoogleMaps_image"></a>`;
 
     expect(result).toEqual(answer);
   });
