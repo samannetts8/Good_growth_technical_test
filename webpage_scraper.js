62bba08d18d99703d4fd60b19ac429ccde6fc1de
samannetts8 <samannetts8@gmail.com>
2025-03-20 13:59:43 +0000

    Embedded Google Maps Tab isolated

--- webpage_scraper.js
+++ webpage_scraper.js
@@ -1,13 +1,19 @@
 import path from "path";
 import fs from "fs";
 import { fileURLToPath } from "url";
+import { JSDOM } from "jsdom";
 
-export default function lonlatRetriever(html) {
-  const output = {
-    longitude: 52.3465,
-    latitude: -1.74551,
-  };
-  return output;
+export default function lonlatRetriever(page_source) {
+  //JSDOM is used to convert the example HTML into DOM structure
+  const DOM_page_source = new JSDOM(page_source);
+  const DOM_document = DOM_page_source.window.document;
+
+  const google_map_tag = DOM_document.getElementById(
+    "propertyViewOnGoogleMaps_image"
+  );
+  const reduced_html_tag = google_map_tag.cloneNode(false).outerHTML;
+  console.log(google_map_tag.cloneNode(false).outerHTML);
+  return reduced_html_tag;
 }
 
 // const google_map_element = html.getElementbyId("propertyViewOnGoogleMaps_image")
--- webpage_scraper.test.js
+++ webpage_scraper.test.js
@@ -2,17 +2,37 @@ import { describe, it, expect } from "vitest";
 import webpage_scraper from "./webpage_scraper";
 import fs from "fs";
 
-//Parsing through example html
-const html_file = fs.readFileSync(
-  new URL("./packwood_house_page_source.html", import.meta.url),
-  "utf8"
-);
+// describe("Longitude, Latitude Retrieval", () => {
+//   it("should return Lon: 52.3465, Lat: -1.74551", () => {
+//     //Arrange
+//     const html_file = fs.readFileSync(
+//       new URL("./packwood_house_page_source.html", import.meta.url),
+//       "utf8"
+//     );
+//     //Act
+//     const result = webpage_scraper(html_file);
+//     //Assert
+//     const answer = {
+//       longitude: 52.3465,
+//       latitude: -1.74551,
+//     };
+
+//     expect(result).toEqual(answer);
+//   });
+// });
 
 describe("Longitude, Latitude Retrieval", () => {
-  it("should return Lon: 52.3465, Lat: -1.74551", () => {
-    expect(webpage_scraper(html_file)).toEqual({
-      longitude: 52.3465,
-      latitude: -1.74551,
-    });
+  it("should return the whole Google Maps element", () => {
+    //Arrange
+    const html_file = fs.readFileSync(
+      new URL("./packwood_house_page_source.html", import.meta.url),
+      "utf8"
+    );
+    //Act
+    const result = webpage_scraper(html_file);
+    //Assert
+    const answer = `<a href="https://www.google.com/maps/dir/?api=1&amp;destination=52.3465%2C-1.74551" id="propertyViewOnGoogleMaps_image"></a>`;
+
+    expect(result).toEqual(answer);
   });
 });
