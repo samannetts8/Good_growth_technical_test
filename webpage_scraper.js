43cfe7df4b1be6b70af9b6a338bec87827fe7c2b
samannetts8 <samannetts8@gmail.com>
2025-03-20 14:39:59 +0000

    Improved readability of webpage_scraper function

--- webpage_scraper.js
+++ webpage_scraper.js
@@ -3,23 +3,29 @@ import fs from "fs";
 import { fileURLToPath } from "url";
 import { JSDOM } from "jsdom";
 
-const html_file = fs.readFileSync(
-  new URL("./packwood_house_page_source.html", import.meta.url),
-  "utf8"
-);
+function handleError(message) {
+  console.error(`Error: ${message}`);
+  return null;
+}
 
 export default function lonlatRetriever(html_file) {
   //JSDOM is used to convert the example HTML into DOM structure
-  const DOM_page_source = new JSDOM(html_file);
-  const DOM_document = DOM_page_source.window.document;
+  const DOM_document = new JSDOM(html_file).window.document;
 
   //find google maps element in HTML
   const google_map_tag = DOM_document.getElementById(
     "propertyViewOnGoogleMaps_image"
   );
 
+  if (!google_map_tag) {
+    handleError("No Google Maps Element Detected");
+  }
+
   // isolate top level of tag, exclude any child elements
   const reduced_html_tag = google_map_tag.cloneNode(false).outerHTML;
+  if (!reduced_html_tag) {
+    handleError("Google Maps child elements not removed");
+  }
 
   //find location of latitude in textAlign:
   const lat_start_index = reduced_html_tag.search("destination=") + 12;
@@ -29,10 +35,16 @@ export default function lonlatRetriever(html_file) {
   const property_latitude = Number(
     reduced_html_tag.substring(lat_start_index, lat_end_index)
   );
+  if (!property_latitude) {
+    handleError("Property latitude not found");
+  }
 
   const property_longitude = Number(
     reduced_html_tag.substring(lat_end_index + 3, lon_end_index)
   );
+  if (!property_longitude) {
+    handleError("Property longitude not found");
+  }
 
   const result = {
     latitude: property_latitude,
@@ -40,7 +52,5 @@ export default function lonlatRetriever(html_file) {
   };
   return result;
 }
-
-console.log(lonlatRetriever(html_file));
 
 // const google_map_element = html.getElementbyId("propertyViewOnGoogleMaps_image")
